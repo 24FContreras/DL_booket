2712379cfcf0ee9e17395647870a8c62941a3730
Franco Contreras~ <24.f.contreras@gmail.com>
2023-06-28 15:55:21 -0400

    changes in product card layout

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,31 +3,32 @@ import { Link } from "react-router-dom";
 import { utils } from "../utils";
 
 const ProductCard = ({ book }) => {
-  const { titulo, autor, precio, editorial, estado, portada, id, imageUrl } =
-    book;
+  const { titulo, autor, precio, editorial, estado, id, imageUrl } = book;
 
   return (
-    <div className="product-card">
-      <div className="product-card-header">
+    <div className="product-card card h-100 border-0">
+      <div className="product-card-header bg-secondary-subtle">
         <img src={imageUrl} alt={titulo} className="shadow-sm" />
         <div className="product-deco"></div>
       </div>
 
-      <div className="product-card-body shadow-sm bg-white p-4">
-        <div className="d-flex flex-row justify-content-between gap-3">
-          <div>
-            <p className="fw-semibold m-0 text-uppercase fs-5">{titulo}</p>
-            <p className="fw-light m-0">{autor}</p>
-            <p className="fw-light my-1 text-details">
-              Editorial {editorial} - {estado}
+      <div className="product-card-body h-100 shadow-sm bg-white p-4 d-flex flex-column justify-content-between">
+        <div>
+          <div className="d-flex flex-row justify-content-between gap-4">
+            <div>
+              <p className="fw-semibold m-0 text-uppercase fs-5">{titulo}</p>
+              <p className="fw-light m-0">{autor}</p>
+            </div>
+            <p className="fs-4 fw-semibold m-0">
+              {utils.formatearDivisa(precio)}
             </p>
           </div>
-          <p className="fs-4 fw-semibold m-0">
-            {utils.formatearDivisa(precio)}
+          <p className="fw-light my-1 text-details">
+            Editorial {editorial} - {estado}
           </p>
         </div>
 
-        <Link className="btn btn-primary mb-1 w-100" to={`/products/${id}`}>
+        <Link className="btn btn-primary w-100" to={`/products/${id}`}>
           Ver detalles
         </Link>
       </div>
